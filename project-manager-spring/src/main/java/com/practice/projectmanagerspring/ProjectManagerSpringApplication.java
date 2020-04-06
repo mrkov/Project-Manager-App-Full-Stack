c61137245d3eea99485117a63a1cb4174027ef26
mrkov <60403788+mrkov@users.noreply.github.com>
2020-04-06 12:01:16 +0200

    first commit

---
+++ project-manager-spring/src/main/java/com/practice/projectmanagerspring/ProjectManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.practice.projectmanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerSpringApplication.class, args);
+	}
+
+}
---
+++ project-manager-spring/src/test/java/com/practice/projectmanagerspring/ProjectManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.projectmanagerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
