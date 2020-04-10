72af025884e0022fdeeaf9deef0e098d200e3f20
mrkov <60403788+mrkov@users.noreply.github.com>
2020-04-10 22:05:45 +0200

    react routing

---
+++ project-manager-react/src/components/Project/AddProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddProject extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Add Project Form</h2>
+            </div>
+        )
+    }
+}
